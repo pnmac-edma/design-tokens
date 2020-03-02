b18ae91e3a51c40809bd328a33b5ee2fad0c301d
Scott Fowles <scott.fowles@pnmac.com>
2020-03-02 07:27:46 -0800

    Update index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import borderRadius from './js/borderRadius';
+import borderSize from './js/borderSize';
+import brand from './js/brand';
+import breakpoint from './js/breakpoint';
+import color from './js/color';
+import font from './js/font';
+import fontSize from './js/fontSize';
+import lineHeight from './js/lineHeight';
+import shadow from './js/shadow';
+import spacing from './js/spacing';
+import weight from './js/weight';
+import z from './js/z';
+
+export {
+    borderRadius,
+    borderSize,
+    brand,
+    breakpoint,
+    color,
+    font,
+    fontSize,
+    lineHeight,
+    shadow,
+    spacing,
+    weight,
+    z,
+}
